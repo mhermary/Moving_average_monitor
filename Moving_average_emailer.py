5fe497728afc1a31bf5373a1e5138c812741e08e
Matthew Hermary <matthermary@gmail.com>
2024-12-21 22:33:23 -0800

    Cleaned code, only sends emails on crossing of MA by comparing last close to prior close

--- Moving_average_emailer.py
+++ Moving_average_emailer.py
@@ -19,39 +19,44 @@ def load_environment_variables(env_file=".env"):
     except FileNotFoundError:
         raise FileNotFoundError(f"{env_file} file not found. Please ensure it exists in the script directory.")
     
-def get_moving_avg():
+def compare_moving_avg():
+    #compares moving average to day before so emails are only sent on crossing of the moving average
+    cross_mov_avg_status = False
+    
+    return cross_mov_avg_status
+
+def moving_avg_status(ticker1, ticker2, period, moving_avg_days):
     # Fetch stock info
     try:
-        moving_avg_days = NUM_MA_DAYS
-        ticker1 = '^GSPC' # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
-        ticker2 = '^NDX' 
-        period = '2y'
         indx1 = yf.Ticker(ticker1) # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
         indx2 = yf.Ticker(ticker2) # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
         indx1_data = indx1.history(period = period) # Index data to track, '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'
         indx2_data = indx2.history(period = period) # Index data to track
         indx1_data['MA'] = indx1_data['Close'].rolling(window=moving_avg_days).mean()
         indx2_data['MA'] = indx2_data['Close'].rolling(window=moving_avg_days).mean()
-
-        moving_avg_msg1 = ""
-        moving_avg_msg2 = ""
-
-    # IS BELOW
-        if indx1_data['Close'].iloc[-1] < indx1_data['MA'].iloc[-1]:
-            moving_avg_msg1 = "S&P500 IS TRADING BELOW " + str(moving_avg_days) +" DAY MOVING AVERAGE."
-        if indx2_data['Close'].iloc[-1] < indx2_data['MA'].iloc[-1]:
-            moving_avg_msg2 = "NASDAQ IS TRADING BELOW " + str(moving_avg_days) +" DAY MOVING AVERAGE."
-        moving_avg_msg = moving_avg_msg1 + "\n" + moving_avg_msg2 + "\n"
-    # IS ABOVE
-        if indx1_data['Close'].iloc[-1] > indx1_data['MA'].iloc[-1]:
-            moving_avg_msg1 = "S&P500 IS TRADING ABOVE " + str(moving_avg_days) +" DAY MOVING AVERAGE."
-        if indx2_data['Close'].iloc[-1] > indx2_data['MA'].iloc[-1]:
-            moving_avg_msg2 = "NASDAQ IS TRADING ABOVE " + str(moving_avg_days) +" DAY MOVING AVERAGE."
-        moving_avg_msg = moving_avg_msg1 + "\n" + moving_avg_msg2 + "\n"
-        return moving_avg_msg
-    
+        spy_above = indx1_data['Close'].iloc[-1] > indx1_data['MA'].iloc[-1]
+        ndx_above = indx2_data['Close'].iloc[-1] > indx2_data['MA'].iloc[-1]
+        spy_above_y = indx1_data['Close'].iloc[-2] > indx1_data['MA'].iloc[-2]
+        ndx_above_y = indx2_data['Close'].iloc[-2] > indx2_data['MA'].iloc[-2]
+        return spy_above, ndx_above, spy_above_y, ndx_above_y
+        
     except Exception as e:
-        return f"Stock information is currently unavailable. Error: {e}"
+        print(f"Error in moving_avg_status: {e}")
+        return None, None, None, None
+
+
+def create_msg(spy, ndx):
+    try:
+        if spy is None or ndx is None:
+             raise ValueError("Invalid input: None Type detected where boolean was expected.")
+        msg = ''
+        spy_stat = 'ABOVE' if spy else 'BELOW'
+        ndx_stat = 'ABOVE' if ndx else 'BELOW'
+        msg = "S&P500 IS TRADING " + spy_stat + ' ' + str(NUM_MA_DAYS) +" DAY MOVING AVERAGE. \n" + "NASDAQ IS TRADING " + ndx_stat + ' ' + str(NUM_MA_DAYS) +" DAY MOVING AVERAGE.\n"
+        return msg
+    except ValueError as e:
+        print(f"Error in create_msg: {e}")
+        return f"Error in create_msg: {e}"
 
 def send_email(
     sender, recipient, subject, message_body, smtp_server, smtp_port, password
@@ -62,8 +67,7 @@ def send_email(
         email["From"] = sender
         email["To"] = recipient
         email["Subject"] = subject
-        email.set_content(message_body)
-        
+        email.set_content(message_body)     
 
         with smtplib.SMTP(smtp_server, port=smtp_port) as smtp:
             smtp.starttls()
@@ -75,19 +79,28 @@ def send_email(
 
 def main():
     load_environment_variables()
-
+    ticker1 = '^GSPC' # ^GSPC = S&P500, ^NDX = NASDAQ, ^DJI = Dow Jones
+    ticker2 = '^NDX' 
+    period = '2y'
+    moving_avg_days = NUM_MA_DAYS
+    
     # Retrieve sensitive data
     sender = os.getenv("EMAIL_SENDER")
     password = os.getenv("EMAIL_PASSWORD")
     SMTP_SERVER = "smtp.gmail.com"
     SMTP_PORT = 587
-    Moving_avg_message = get_moving_avg()
+    SPY_status, NDX_status, SPY_stat_y, NDX_stat_y = moving_avg_status(ticker1, ticker2, period, moving_avg_days)
+    if SPY_status != SPY_stat_y or NDX_status != NDX_stat_y:
+        moving_avg_message = create_msg(SPY_status, NDX_status)
+    else: 
+        print("No change in moving average side, terminating program")
+        return
 
     # Create the email body with separated sections
     message_body = (
         "Good morning! Here's your update:\n\n"
         "---- MOVING AVERAGE REPORT ----\n\n"
-        f"{Moving_avg_message}\n"
+        f"{moving_avg_message}\n"
     )
 
     # Send the email to the sender
